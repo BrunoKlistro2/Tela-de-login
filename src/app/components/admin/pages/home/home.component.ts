c2ef0ad01c19867fba648b379b23dcc099122e74
Bruno Calistro <BrunoCalistro4002@hotmail.com>
2023-03-22 16:26:41 -0300

    Projeto de uma tela de login

---
+++ src/app/components/admin/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+
+  constructor(private authService: AuthService){
+
+  }
+
+  public logOut(){
+    this.authService.logOut()
+  }
+}
